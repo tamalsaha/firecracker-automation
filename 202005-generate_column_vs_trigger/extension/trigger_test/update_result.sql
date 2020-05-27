7c7be525ddd891b1666509e25e5e699fb0bfe7e1
asotolongo <asotolongo@gmail.com>
2020-05-27 18:32:42 -0400

    delete extension directory

--- 202005-generate_column_vs_trigger/extension/trigger_test/update_result.sql
+++
@@ -1,8 +0,0 @@
-select calls, 
-round(total_time::numeric,2) as total_time,
-round(min_time::numeric,2) as min_time,
-round(max_time::numeric,2)as  max_time,
-round(mean_time::numeric,2) as mean_time,
-round(stddev_time::numeric,2) as stddev_time,
-query::character varying(30) 
-from pg_stat_statements where query like '%update square_%' order by 7 ;
