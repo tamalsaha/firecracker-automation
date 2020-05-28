0a1c51d286dd55e4d99c9358552a4e1e9a57ed55
3manuek <emanuel@ongres.com>
2020-05-27 21:32:35 -0300

    Readability change on init.sh

--- 202005-generate_column_vs_trigger/init.sh
+++ 202005-generate_column_vs_trigger/init.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
+
 [ -z $(docker-compose ps -q ) ] && docker-compose up -d
 
 
-docker-compose exec my_postgres12 /bin/bash -c  "apt-get update && apt-get install -y make gcc postgresql-server-dev-12 && \
- echo autovacuum=\'off\' >> /var/lib/postgresql/data/postgresql.conf  &&\
- echo shared_preload_libraries=\'pg_stat_statements\' >> /var/lib/postgresql/data/postgresql.conf &&\
- echo shared_buffers=\'256MB\' >> /var/lib/postgresql/data/postgresql.conf &&\
- echo max_wal_size=\'5GB\' >> /var/lib/postgresql/data/postgresql.conf  &&\
- echo checkpoint_timeout=\'30 min\' >> /var/lib/postgresql/data/postgresql.conf" 
+docker-compose exec my_postgres12 /bin/bash -c  "apt-get update && apt-get install -y make gcc postgresql-server-dev-12" 
+        
+
+cat > ./pg_data/postgresql.conf << EOF        
+ autovacuum='off'
+ shared_preload_libraries='pg_stat_statements'
+ shared_buffers='256MB'
+ max_wal_size='5GB'
+ checkpoint_timeout='30 min' 
+EOF
 
 docker-compose exec my_postgres12 /bin/bash -c  "cd /trigger_test && make && make install"
 
