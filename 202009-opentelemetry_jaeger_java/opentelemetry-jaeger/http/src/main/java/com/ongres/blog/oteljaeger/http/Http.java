7775b3dd8988d5b6a8e19ac5b5fcc25e520e8bbf
Álvaro Hernández <aht@ongres.com>
2020-09-21 12:50:50 +0200

    Turned the project into a multi-module Quarkus app
    The project now contains two modules:
    
    * main: QuarkusMain. Prepared to perform configuration and
      initialization.
    
    * http: web services to be traced
    
    This two-module structure is created to show when tracing is added how
    the TracingProvider and configuration is (should be) split from any
    other module just performing tracing, which will depend only on the
    opentel tracing API.
    
    [202009-opentelemetry_jaeger_java]
    
    Signed-off-by: Álvaro Hernández <aht@ongres.com>

--- 202009-opentelemetry_jaeger_java/opentelemetry-jaeger/http/src/main/java/com/ongres/blog/oteljaeger/http/Http.java
+++ 202009-opentelemetry_jaeger_java/opentelemetry-jaeger/http/src/main/java/com/ongres/blog/oteljaeger/http/Http.java
@@ -1,9 +1,10 @@
-package com.ongres.blog.oteljaeger;
+package com.ongres.blog.oteljaeger.http;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("/traceme")
 public class Http {
